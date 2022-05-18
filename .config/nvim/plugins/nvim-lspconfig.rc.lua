0b1116e319b5e7bd17c67087483d5b12668d086c
reona5 <shimada.reona@gmail.com>
2022-05-18 23:39:25 +0900

    Add lspconfig settings for gopls.

--- .config/nvim/plugins/nvim-lspconfig.rc.lua
+++ .config/nvim/plugins/nvim-lspconfig.rc.lua
@@ -205,3 +205,18 @@ require'lspconfig'.solargraph.setup{
     }
   }
 }
+
+-- gopls
+require'lspconfig'.gopls.setup {
+  cmd = {"gopls", "serve"},
+  filetypes = {"go", "gomod"},
+  root_dir = require'lspconfig.util'.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
