5f6b8f02e9a948f067e895e1d994fed8ee433483
reona5 <shimada.reona@gmail.com>
2022-05-18 23:35:42 +0900

    Enable to insert imported library for golang.

--- .config/nvim/plugins/nvim-lspconfig.rc.lua
+++ .config/nvim/plugins/nvim-lspconfig.rc.lua
@@ -1,5 +1,22 @@
 -- nvim-lspconfig
 local on_attach = function(_, bufnr)
+  -- Enable to insert imported library for golang
+  function OrgImports(wait_ms)
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+
+  vim.api.nvim_command [[autocmd BufWritePre *.go lua OrgImports(100)]]
   vim.api.nvim_command [[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)]]
   vim.api.nvim_command [[autocmd BufWritePre *.js lua vim.lsp.buf.formatting_sync(nil, 1000)]]
   vim.api.nvim_command [[autocmd BufWritePre *.ts lua vim.lsp.buf.formatting_sync(nil, 1000)]]
