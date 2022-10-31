aa12d071908cdeab498762025e71c5b26c56c861
zys <zaffys@outlook.com>
2022-10-31 13:58:29 -0400

    added installs, fix tmux copy/paste, misc

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,3 +1,16 @@
+### Added by Zinit's installer
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
 zinit wait lucid blockf light-mode for \
   @'zsh-users/zsh-autosuggestions' \
   @'zdharma-continuum/fast-syntax-highlighting' \
@@ -15,6 +28,26 @@ zinit light-mode for \
     zdharma-continuum/z-a-patch-dl \
     zdharma-continuum/z-a-bin-gem-node
 
+zinit ice from"gh-r" as"program" atload'!eval $(starship init zsh)'
+zinit light starship/starship
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma-continuum/fast-syntax-highlighting
+zinit light zsh-users/zsh-history-substring-search
+zinit ice wait'0'; zinit light zsh-users/zsh-completions
+autoload -Uz compinit && compinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/z-a-rust \
+    zdharma-continuum/z-a-as-monitor \
+    zdharma-continuum/z-a-patch-dl \
+    zdharma-continuum/z-a-bin-gem-node
+
+zinit wait lucid light-mode as'null' \
+    atinit'source "$HOME/.zshrc.lazy"' \
+    for 'zdharma-continuum/null'
+    
 zinit ice from"gh-r" as"program" atload'!eval $(starship init zsh)'
 zinit light starship/starship
 zinit light zsh-users/zsh-autosuggestions
