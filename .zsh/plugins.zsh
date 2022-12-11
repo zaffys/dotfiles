b7fd4f772502a43863f68e259e022b5c62fbca5d
zys <zaffys@outlook.com>
2022-12-11 11:46:11 -0500

    merge fixes

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -12,24 +12,12 @@ zinit wait lucid blockf light-mode for \
   @'zsh-users/zsh-completions' \
   @'djui/alias-tips' \
 
-source "$HOME/.zinit/bin/zinit.zsh"
-autoload -Uz _zinit
-(( ${+_comps} )) && _comps[zinit]=_zinit
-
-# Load a few important annexes, without Turbo
-# (this is currently required for annexes)
-zinit light-mode for \
-    zdharma-continuum/z-a-rust \
-    zdharma-continuum/z-a-as-monitor \
-    zdharma-continuum/z-a-patch-dl \
-    zdharma-continuum/z-a-bin-gem-node
-
-zinit ice from"gh-r" as"program" atload'!eval $(starship init zsh)'
-zinit light starship/starship
-zinit light zsh-users/zsh-autosuggestions
-zinit light zdharma-continuum/fast-syntax-highlighting
-zinit light zsh-users/zsh-history-substring-search
-zinit ice wait'0'; zinit light zsh-users/zsh-completions
-autoload -Uz compinit && compinit
+# NOTE: https://gist.github.com/ctechols/ca1035271ad134841284
+autoload -Uz compinit
+if [[ -n ${ZDOTDIR}/.zcompdump(#qN.mh+24) ]]; then
+	compinit;
+else
+	compinit -C;
+fi;
 
 eval "$(direnv hook zsh)"
